c2119d5ea1d5255624569588eb61cdf0aeec3f74
huda-mokhtar <hodaheikal6@gmail.com>
2021-02-03 01:06:12 +0200

    ..

--- src/app/_service/user.service.ts
+++ src/app/_service/user.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
+  register(user:User){
+    return this.http.post<User>(" https://blogsuser.herokuapp.com/users",user);
+  } 
 }
