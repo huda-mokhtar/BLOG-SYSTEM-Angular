aa58893b94c4a9473ec1e4e9c420011fce373678
wafaaElsaeed <77937661+wafaaElsaeed@users.noreply.github.com>
2021-02-12 13:11:59 +0200

    oo

--- src/app/author-blogs/author-blogs.component.ts
+++ src/app/author-blogs/author-blogs.component.ts
@@ -14,10 +14,28 @@ export class AuthorBlogsComponent implements OnInit {
   blogs:Blogs[];
   deletedBlog :Blogs;
   
+
+  title = 'appBootstrap';
+  
+  closeResult: string;
   constructor(public blogsservice:BlogsService ,  public ar:ActivatedRoute, public route:Router , private modalService: NgbModal) { }
 
-  openVerticallyCentered(content) {
-    this.modalService.open(content, { centered: true });
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 
